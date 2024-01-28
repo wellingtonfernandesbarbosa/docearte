784f38888455da59fbac7bfefb9e872652fa074a
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-01-28 11:36:41 -0300

    Form Refactoring

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-import { toggleMenu } from "./app/toggleMenu.js";
-import { phoneInput } from "./app/phoneInput.js";
+const nameInput = document.querySelector("#name");
+const dateInput = document.querySelector("#date");
+const eventInput = document.querySelector("#event");
+const guestsInput = document.querySelector("#guests");
+const preferencesInput = document.querySelector("#preferences");
+
+const formButton = document.querySelector("#formButton");
+const phoneNumber = 5531996964513;
 
 document.addEventListener("DOMContentLoaded", function () {
-  toggleMenu();
-  phoneInput();
+  nameInput.addEventListener('input', () => {
+    messageFormatting();
+  })
+  
+  dateInput.addEventListener('input', () => {
+    messageFormatting();
+  })
+  
+  eventInput.addEventListener('input', () => {
+    messageFormatting();
+  })
+  
+  guestsInput.addEventListener('input', () => {
+    messageFormatting();
+  })
 
-  const form = document.querySelector("#form");
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  preferencesInput.addEventListener("input", () => {
+    messageFormatting();
   });
 });
+
+function messageFormatting() {
+  const message = `Olá Doce Artes!
+
+    Estou interessado em encomendar um bolo especial para uma ocasião especial. Aqui estão os detalhes:
+
+    Tipo de Evento: ${eventInput.value}
+    Data do Evento: ${dateInput.value}
+    Número de Convidados: ${guestsInput.value}
+    Ideias ou Preferências para o Bolo: ${preferencesInput.value}
+
+    Gostaria de saber se vocês têm disponibilidade para essa data e um orçamento aproximado. Fico no aguardo de sua resposta!
+
+    Obrigado,
+    ${nameInput.value}.`;
+
+  formButton.setAttribute('href', `https://wa.me/${phoneNumber}?text=${message}`);
+}
+
