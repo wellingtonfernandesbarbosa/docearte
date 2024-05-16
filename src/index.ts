54a632c40ceeccd727c1b8fa1222c365c8c38501
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-05-16 17:29:17 -0300

    Redesigned navigation bar and added haptic feedback to the menu icon

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { formMessage } from "./app/formMessage.js";
 import { isClickInsideMenu } from "./app/menu.js";
 
 const menu = document.querySelector("#right-menu") as HTMLUListElement;
+const hanburguerIcon = document.querySelector(".menu-toggle") as HTMLDivElement;
 
 formMessage();
 
 menu.addEventListener("click", (event) => {
   isClickInsideMenu(event);
 });
+
+hanburguerIcon.addEventListener("click", () => {
+  navigator.vibrate && navigator.vibrate(50);
+})
