89e46f2efa5e3c766ba790990644693e133d9749
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-05-17 15:52:25 -0300

    Adjusted navigation link color and fixed bug preventing mobile navigation menu from opening

--- public/index.js
+++ public/index.js
@@ -1,11 +1,7 @@
 import { formMessage } from "./app/formMessage.js";
 import { isClickInsideMenu } from "./app/menu.js";
 const menu = document.querySelector("#right-menu");
-const hanburguerIcon = document.querySelector(".menu-toggle");
 formMessage();
 menu.addEventListener("click", (event) => {
     isClickInsideMenu(event);
-});
-hanburguerIcon.addEventListener("click", () => {
-    navigator.vibrate && navigator.vibrate(50);
 });
