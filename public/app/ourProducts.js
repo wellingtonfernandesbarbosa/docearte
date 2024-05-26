8d80a83ba1324d584c446e84dfa182e6618a811d
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-05-26 19:18:02 -0300

    Refactored the "Our Catalog" section

---
+++ public/app/ourProducts.js
@@ -0,0 +1,28 @@
+"use strict";
+const cakes = document.getElementById("cakes");
+const sweets = document.getElementById("sweets");
+const cakesBtn = document.getElementById("cakesBtn");
+const sweetsBtn = document.getElementById("sweetsBtn");
+const cakesInfo = document.getElementById("cakesInfo");
+const sweetsInfo = document.getElementById("sweetsInfo");
+const cakeLinkButton = document.getElementById("cakeLinkButton");
+const cupcakesLinkButton = document.getElementById("cupcakeLinkButton");
+const sweetsLinkButton = document.getElementById("sweetLinkButton");
+const cakeLink = `https://wa.me/5531997706644?text=${encodeURIComponent("Olá, gostarida de saber mais sobre os bolos disponíveis.")}`;
+cakeLinkButton?.setAttribute("href", cakeLink);
+const sweetLink = `https://wa.me/5531997706644?text=${encodeURIComponent("Olá, gostarida de saber mais sobre os doces e bombons disponíveis.")}`;
+sweetsLinkButton?.setAttribute("href", sweetLink);
+cakesBtn?.addEventListener("click", () => {
+    navigator.vibrate(50);
+    cakesInfo?.setAttribute("style", "display: flex");
+    sweetsInfo?.setAttribute("style", "display: none");
+    cakes?.setAttribute("style", "display: flex");
+    sweets?.setAttribute("style", "display: none");
+});
+sweetsBtn?.addEventListener("click", () => {
+    navigator.vibrate(50);
+    cakesInfo?.setAttribute("style", "display: none");
+    sweetsInfo?.setAttribute("style", "display: flex");
+    cakes?.setAttribute("style", "display: none");
+    sweets?.setAttribute("style", "display: flex");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { formMessage } from "./app/formMessage.js";
 import { isClickInsideMenu } from "./app/menu.js";
+import "./app/ourProducts.js";
 
 const menu = document.querySelector("#right-menu") as HTMLUListElement;
 
