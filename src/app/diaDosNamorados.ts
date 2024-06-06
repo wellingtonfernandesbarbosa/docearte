313c7047e6fb90c66e597e35129bbf554cde4b52
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-06 18:13:15 -0300

    Added flash sale to the Valentine's Day section

--- src/app/diaDosNamorados.ts
+++ src/app/diaDosNamorados.ts
@@ -2,6 +2,16 @@ const diaDosNamoradosButton = document.querySelector('#diaDosNamoradosButton') a
 const diaDosNamoradosSection = document.querySelector('.diaDosNamorados-section') as HTMLDivElement;
 const diaDosNamoradosCallToAction = document.querySelector('.diaDosNamorados-callToAction') as HTMLDivElement;
 
+const flashSaleButton = document.querySelector('#flashSaleButton') as HTMLButtonElement;
+const flashSalecallToAction = document.querySelector('.flashSale-callToAction') as HTMLDivElement;
+
+flashSaleButton.addEventListener('click', () => {
+  flashSalecallToAction.style.display = 'none';
+  diaDosNamoradosSection.style.display = 'flex';
+  diaDosNamoradosCallToAction.style.display = 'none';
+  window.location.href = '#flashSale';
+})
+
 diaDosNamoradosButton.addEventListener('click', () => {
   diaDosNamoradosCallToAction.style.display = 'none';
   diaDosNamoradosSection.style.display = 'flex';
