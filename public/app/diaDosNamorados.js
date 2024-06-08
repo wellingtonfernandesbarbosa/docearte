9fa63bc6d9aa8b674ad2fe1c4f1bf8e24e64a79a
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-08 09:11:18 -0300

    remoção de promoção

--- public/app/diaDosNamorados.js
+++ public/app/diaDosNamorados.js
@@ -2,6 +2,14 @@
 const diaDosNamoradosButton = document.querySelector('#diaDosNamoradosButton');
 const diaDosNamoradosSection = document.querySelector('.diaDosNamorados-section');
 const diaDosNamoradosCallToAction = document.querySelector('.diaDosNamorados-callToAction');
+const flashSaleButton = document.querySelector('#flashSaleButton');
+const flashSalecallToAction = document.querySelector('.flashSale-callToAction');
+flashSaleButton.addEventListener('click', () => {
+    flashSalecallToAction.style.display = 'none';
+    diaDosNamoradosSection.style.display = 'flex';
+    diaDosNamoradosCallToAction.style.display = 'none';
+    window.location.href = '#flashSale';
+});
 diaDosNamoradosButton.addEventListener('click', () => {
     diaDosNamoradosCallToAction.style.display = 'none';
     diaDosNamoradosSection.style.display = 'flex';
