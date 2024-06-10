152d08f2f32cfbd1a7f8ae64f656db267ab14bc6
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-10 13:16:19 -0300

    Removed Valentine's Day section

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { formMessage } from "./app/formMessage.js";
 import { isClickInsideMenu } from "./app/menu.js";
 import "./app/ourProducts.js";
-import "./app/diaDosNamorados.js";
 const menu = document.querySelector("#right-menu");
 formMessage();
 menu.addEventListener("click", (event) => {
