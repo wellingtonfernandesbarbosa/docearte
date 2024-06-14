817fc93a5733a7d22987cc60ee03eb0c7dcb1068
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-14 15:00:55 -0300

    Adjust menu icon size and implement haptic feedback

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -1,3 +1,5 @@
+import hapticFeedback from "./utils/hapticFeedback";
+
 const menuToggle = document.querySelector(".menu-toggle") as HTMLDivElement;
 const menu = document.querySelector("#right-menu") as HTMLUListElement;
 const menuLinks = document.querySelectorAll("#right-menu li a") as NodeListOf<HTMLAnchorElement>;
@@ -8,6 +10,7 @@ export const isClickInsideMenu = (event: MouseEvent | TouchEvent) => {
 }
 
 menuToggle?.addEventListener('click', () => {
+  hapticFeedback();
   if (screenWidth <= 900) menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 });
 
@@ -21,6 +24,9 @@ menuLinks.forEach((link) => {
   screenWidth <= 850 && link.addEventListener('click', () => {
     menu.style.display = 'none';
   });
+  link.addEventListener('click', () => {
+    hapticFeedback();
+  })
 })
 
 menuToggle.addEventListener("click", () => {
