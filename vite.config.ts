da5c02ed830b937ffc4f18dd409520bf362313b3
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-27 11:46:26 -0300

    Updated project settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   build: {
     rollupOptions: {
       input: {
