358b10eccae4346cd3e9ad3c34405706d81800f6
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-27 11:54:25 -0300

    Removed unused files

--- public/app/diaDosNamorados.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const diaDosNamoradosButton = document.querySelector('#diaDosNamoradosButton');
-const diaDosNamoradosSection = document.querySelector('.diaDosNamorados-section');
-const diaDosNamoradosCallToAction = document.querySelector('.diaDosNamorados-callToAction');
-diaDosNamoradosButton.addEventListener('click', () => {
-    diaDosNamoradosCallToAction.style.display = 'none';
-    diaDosNamoradosSection.style.display = 'flex';
-});
--- public/app/testeHello.js
+++
@@ -1,3 +0,0 @@
-export const printHello = () => {
-    return console.log("Hello, World!");
-};
