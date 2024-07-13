b9b2466ecb3c6f102102f611c54679fb829f302b
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-07-13 09:13:17 -0300

    Implemented button to show information about curiosities

--- src/app/curiosities.ts
+++ src/app/curiosities.ts
@@ -59,3 +59,13 @@ curiositiesEggs.addEventListener('click', () => {
   curiositiesTitle.innerHTML = curiosities[2].title;
   curiositiesText.innerHTML = curiosities[2].text;
 })
+
+
+const curiositiesButton = document.getElementById('curiositiesButton') as HTMLButtonElement;
+
+curiositiesButton.addEventListener('click', () => {
+  hapticFeedback();
+  curiositiesTitle.setAttribute('style', 'display: block');
+  curiositiesText.setAttribute('style', 'display: block');
+  curiositiesButton.setAttribute('style', 'display: none');
+});
