7e84c2d789fecce2e77ee3ddf289386f975db9a7
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-07-18 09:06:41 -0300

    Fixed bug in mobile menu

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -3,32 +3,29 @@ import hapticFeedback from "./utils/hapticFeedback";
 const menuToggle = document.querySelector(".menu-toggle") as HTMLDivElement;
 const menu = document.querySelector("#right-menu") as HTMLUListElement;
 const menuLinks = document.querySelectorAll("#right-menu li a") as NodeListOf<HTMLAnchorElement>;
-const screenWidth = window.innerWidth;
+
+const closeMenu = () => menu.style.display = 'none';
+
+const openMenu = () => menu.style.display = 'flex';
+
+const widthUnder1000 = () => window.innerWidth <= 1000;
 
 export const isClickInsideMenu = (event: MouseEvent | TouchEvent) => {
   return menu?.contains(event.target as Node) || menuToggle?.contains(event.target as Node);
 }
 
-menuToggle?.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
   hapticFeedback();
-  if (screenWidth <= 900) menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+  if (widthUnder1000()) menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 });
 
-window.addEventListener('click', (event) => {
-  if (screenWidth <= 850 && !isClickInsideMenu(event)) {
-    menu.style.display = 'none';
-  }
-})
+window.addEventListener('click', (event) => (widthUnder1000() && !isClickInsideMenu(event)) && closeMenu());
 
 menuLinks.forEach((link) => {
-  screenWidth <= 850 && link.addEventListener('click', () => {
-    menu.style.display = 'none';
-  });
   link.addEventListener('click', () => {
     hapticFeedback();
+    if (widthUnder1000()) closeMenu();
   })
-})
+});
 
-menuToggle.addEventListener("click", () => {
-  navigator.vibrate && navigator.vibrate(50);
-})
+window.addEventListener('resize', () => window.innerWidth > 1000 ? openMenu() : closeMenu());
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { formMessage } from "./app/formMessage.js";
-import { isClickInsideMenu } from "./app/menu.js";
 import "./app/ourCatalog.js";
 import './app/curiosities.js';
 
-const menu = document.querySelector("#right-menu") as HTMLUListElement;
-
+import { formMessage } from "./app/formMessage.js";
 formMessage();
 
+import { isClickInsideMenu } from "./app/menu.js";
+const menu = document.querySelector("#right-menu") as HTMLUListElement;
 menu.addEventListener("click", (event) => {
   isClickInsideMenu(event);
 });
