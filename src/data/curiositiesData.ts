5ec75bde284a176cacfd41c066a33d53d70df1f3
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-07-31 07:51:50 -0300

    Addition of yeast on the curiosities list

--- src/data/curiositiesData.ts
+++ src/data/curiositiesData.ts
@@ -1,33 +1,76 @@
-export const curiosities = [
+type Curiosities = {
+  id: number
+  img: string
+  nome: string
+  title: string
+  text: string
+}
+
+export const curiosities: Curiosities[] = [
   {
     id: 0,
     img: '/assets/img/curiosities_butter.webp',
     nome: 'Manteiga',
     title: 'A Alma dos Bolos Irresistíveis',
-    text: '🧈✨ Começamos esta série revelando o segredo da nossa principal receita: MASSA AMANTEIGADA. <br> 🍰 A manteiga proporciona uma maciez e leveza incomparáveis, garantindo bolos com sabor inigualável e textura irresistível. <br>🔹 <span>Sabor e Textura</span>: A gordura da manteiga derrete na massa, criando aquela sensação deliciosa de derreter na boca. <br>🔹 <span>Cor e Aroma</span>: Durante o cozimento, a manteiga carameliza, dando uma cor dourada e um aroma tentador! 😍✨<br>🔹 <span>Estrutura</span>: A manteiga ajuda a formar a estrutura do bolo, resultando em uma massa estável e macia.<br>Venha descobrir o que torna nossos bolos tão especiais! Experimente nossos bolos que são puro sabor!😋'
+    text: `🧈✨ Começamos esta série revelando o segredo da nossa principal receita: MASSA AMANTEIGADA. <br><br>
+    🍰 A manteiga proporciona uma maciez e leveza incomparáveis, garantindo bolos com sabor inigualável e textura irresistível. <br><br>
+    🔹 <span>Sabor e Textura</span>: A gordura da manteiga derrete na massa, criando aquela sensação deliciosa de derreter na boca. <br><br>
+    🔹 <span>Cor e Aroma</span>: Durante o cozimento, a manteiga carameliza, dando uma cor dourada e um aroma tentador! 😍✨<br>
+    🔹 <span>Estrutura</span>: A manteiga ajuda a formar a estrutura do bolo, resultando em uma massa estável e macia.<br><br>
+    Venha descobrir o que torna nossos bolos tão especiais!<br>Experimente nossos bolos que são puro sabor!😋`
   }, {
     id: 1,
     nome: 'Acúcar',
     img: '/assets/img/curiosities_sugar.webp',
     title: 'Doçura, Sabor e Textura Perfeita',
-    text: '🍰✨O açúcar é a estrela que adoça o bolo e realça todos os sabores! 😋✨<br>🛡️<span>Estrutura</span>: Sabia que o açúcar ajuda a formar a estrutura perfeita do bolo antes que o amido endureça?<br>Isso garante aquela textura macia e irresistível! 🧁 🌟<br>🛡️<span>Cor e Aroma</span>: Durante o cozimento, o açúcar carameliza, dando aquela cor dourada linda e um aroma irresistível! 😍🌟<br>🛡️ <span>Conservação</span>: Além de tudo, o açúcar ajuda a manter seu bolo fresquinho por mais tempo, dificultando o crescimento de microrganismos! 🍰🛡️'
+    text: `🍰✨O açúcar é a estrela que adoça o bolo e realça todos os sabores! 😋✨<br><br>
+    🛡️ <span>Estrutura</span>: Sabia que o açúcar ajuda a formar a estrutura perfeita do bolo antes que o amido endureça? Isso garante aquela textura macia e irresistível! 🧁 🌟<br><br>
+    🛡️ <span>Cor e Aroma</span>: Durante o cozimento, o açúcar carameliza, dando aquela cor dourada linda e um aroma irresistível! 😍🌟<br><br>
+    🛡️ <span>Conservação</span>: Além de tudo, o açúcar ajuda a manter seu bolo fresquinho por mais tempo, dificultando o crescimento de microrganismos! 🍰🛡️`
   }, {
     id: 2,
     nome: 'Ovos',
     img: '/assets/img/curiosities_eggs.webp',
     title: 'A Magia por Trás da Textura dos Sonhos',
-    text: '🌟✨ Os ovos são o toque mágico para uma textura perfeita e aquela cor dourada irresistível! Vamos descobrir como eles fazem toda a diferença: <br>🔹 <span>Textura Impecável</span>: As claras formam uma rede que dá estrutura, enquanto as gemas adicionam leveza e maciez.<br>O resultado? Um bolo firme e ao mesmo tempo fofinho!<br>🔹 <span>Umidade Perfeita</span>: Ricos em água, os ovos mantêm nossos bolos úmidos e deliciosos, garantindo que cada mordida seja uma experiência suculenta e agradável.<br>🔹 <span>Cor Dourada e Sabor Intenso</span>: Graças à Reação de Maillard, os ovos proporcionam uma cor dourada irresistível e intensificam os sabores, deixando nossos bolos ainda mais saborosos.<br>Experimente a magia dos ovos em nossos bolos e sinta a diferença! 🍰💛'
+    text: `🌟✨ Os ovos são o toque mágico para uma textura perfeita e aquela cor dourada irresistível! Vamos descobrir como eles fazem toda a diferença: <br><br>
+    🔹 <span>Textura Impecável</span>: As claras formam uma rede que dá estrutura, enquanto as gemas adicionam leveza e maciez.<br>O resultado? Um bolo firme e ao mesmo tempo fofinho!<br><br>
+    🔹 <span>Umidade Perfeita</span>: Ricos em água, os ovos mantêm nossos bolos úmidos e deliciosos, garantindo que cada mordida seja uma experiência suculenta e agradável.<br><br>
+    🔹 <span>Cor Dourada e Sabor Intenso</span>: Graças à <span><a href="https://pt.wikipedia.org/wiki/Rea%C3%A7%C3%A3o_de_Maillard#:~:text=A%20rea%C3%A7%C3%A3o%20de%20Maillard%20(%2Fm,outros%20alimentos%20sofrem%20essa%20rea%C3%A7%C3%A3o." title="Reação de Maillard" target="_blank">Reação de Maillard</a></span>, os ovos proporcionam uma cor dourada irresistível e intensificam os sabores, deixando nossos bolos ainda mais saborosos.<br><br>
+    Experimente a magia dos ovos em nossos bolos e sinta a diferença! 🍰💛`
   }, {
     id: 3,
     nome: 'Leite',
     img: '/assets/img/curiosities_milk.webp',
     title: 'O Segredo da Umidade e do Sabor',
-    text: "🥛✨ O leite é o segredo para um bolo perfeito! <br>🥛✨ Ele desempenha um papel crucial nas reações químicas que garantem uma textura macia, um sabor irresistível e aquela cor dourada maravilhosa. <br>🌟🍰 O leite adiciona umidade à massa, o que ajuda a manter o bolo úmido e fofinho.<br>Além disso, suas proteínas contribuem para a estrutura do bolo, ajudando a massa a crescer e a ficar estável. <br>🧁 O leite também melhora a aparência do bolo, dando aquela cor dourada que todos adoram.<br>Sem o leite, não teríamos bolos tão deliciosos e bem estruturados. <br>💕 Quer um bolo incrível ? Não esqueça do leite! 🥛✨"
+    text: `🥛✨ O leite é o segredo para um bolo perfeito! <br>
+    🥛✨ Ele desempenha um papel crucial nas reações químicas que garantem uma textura macia, um sabor irresistível e aquela cor dourada maravilhosa. <br><br>
+    🌟🍰 O leite adiciona umidade à massa, o que ajuda a manter o bolo úmido e fofinho.<br><br>
+    Além disso, suas proteínas contribuem para a estrutura do bolo, ajudando a massa a crescer e a ficar estável.<br><br>
+    🧁 O leite também melhora a aparência do bolo, dando aquela cor dourada que todos adoram.<br>Sem o leite, não teríamos bolos tão deliciosos e bem estruturados.<br><br>
+    🥛✨💕 Quer um bolo incrível ? Não esqueça do leite! 🥛✨`
   }, {
     id: 4,
     nome: 'Farinha de Trigo',
     img: '/assets/img/curiosities_wheatFlour.webp',
     title: 'O alicerce do bolo!',
-    text: '🏗️✨A mágica acontece graças ao glúten, uma proteína poderosa presente no trigo. 🌾✨ Quando misturada com água, ela forma uma rede elástica que prende o ar e dá estrutura à massa. 🔗<br>💪 Essa rede é fundamental para que nossos pães cresçam no forno e os bolos fiquem macios e aerados. 🥖🍰 Sem o glúten, teríamos apenas um amontoado de grãos úmidos. 😅<br><br>😋🌟 Sabor e Aroma Únicos!<br>😋🌟A farinha de trigo não é só estrutura! Ela também adiciona um sabor incrível e aroma irresistível aos nossos pães e bolos, elevando cada mordida a um nível de delícia sem igual. 🍞✨<br><br>🎨👨‍🍳 Versatilidade Sem Limites! 🍕🥟<br>🍪A farinha de trigo vai além do pãozinho de cada dia! Ela é a base para massas de tortas, pizzas, macarrão caseiro, empadas, biscoitos e muito mais. 🥧🍝🥠 Com criatividade, podemos usar a farinha de trigo para criar pratos doces e salgados incríveis! 🎨🍽️<br><br>😋💫 Além de oferecer suporte estrutural, a farinha de trigo contribui para o crescimento, a maciez, o sabor e o aroma do bolo, tornando cada fatia irresistível. 😋💫<br><br>Descubra o potencial mágico da farinha de trigo na sua cozinha! 🔥🎂✨'
+    text: `🏗️✨A mágica acontece graças ao glúten, uma proteína poderosa presente no trigo. 🌾✨ Quando misturada com água, ela forma uma rede elástica que prende o ar e dá estrutura à massa. 🔗<br>
+    💪 Essa rede é fundamental para que nossos pães cresçam no forno e os bolos fiquem macios e aerados. 🥖🍰 Sem o glúten, teríamos apenas um amontoado de grãos úmidos. 😅<br><br>
+    😋🌟 Sabor e Aroma Únicos!<br>😋🌟A farinha de trigo não é só estrutura! Ela também adiciona um sabor incrível e aroma irresistível aos nossos pães e bolos, elevando cada mordida a um nível de delícia sem igual. 🍞✨<br><br>
+    🎨👨‍🍳 Versatilidade Sem Limites! 🍕🥟<br>🍪A farinha de trigo vai além do pãozinho de cada dia! Ela é a base para massas de tortas, pizzas, macarrão caseiro, empadas, biscoitos e muito mais. 🥧🍝🥠 Com criatividade, podemos usar a farinha de trigo para criar pratos doces e salgados incríveis! 🎨🍽️<br><br>
+    😋💫 Além de oferecer suporte estrutural, a farinha de trigo contribui para o crescimento, a maciez, o sabor e o aroma do bolo, tornando cada fatia irresistível. 😋💫<br><br>
+    Descubra o potencial mágico da farinha de trigo na sua cozinha! 🔥🎂✨`
+  },
+  {
+    id: 5,
+    nome: 'Fermento',
+    img: '/assets/img/curiosities_yeast.webp',
+    title: 'Leveduras em Ação',
+    text: `
+    🍞 <span>REAÇÃO QUÍMICA:</span> O fermento contém leveduras, elas se misturam com a umidade e o açúcar na massa. As leveduras "comem" o açúcar e liberam gás carbônico e um pouquinho de álcool. É o gás carbônico que faz a massa crescer, enquanto o álcool evapora no forno, dando aquela textura maravilhosa ao bolo. 😋✨<br><br>
+    💨 <span>FORMAÇÃO DE BOLHAS</span>: O gás carbônico fica preso em pequenas bolhas, fazendo a massa aumentar de volume. 📈🥳<br><br>
+    🎉 <span>TEXTURA MACIA E LEVE</span>: O calor do forno faz as bolhas se expandirem ainda mais, e o álcool some completamente, deixando seu bolo super leve e delicioso! 🍰💖<br><br>
+    PORQUE NÃO SENTIMOS O gosto DE ÁLCOOL NO BOLO?<br>
+    🍷👉 Porque a quantidade de álcool é mínima e evapora totalmente durante o cozimento! Além disso, o sabor é escondido pelo açúcar, manteiga, e outros ingredientes incríveis do bolo. 😋👌<br><br>
+    VAMOS ASSAR ?¿🔥👩‍🍳👨‍🍳`
   }
 ]
